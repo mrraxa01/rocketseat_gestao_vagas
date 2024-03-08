9e50a0f25f0638213c1fcbb86467c88213a75413
Marcio Rodrigues <mrraxa01@gmail.com>
2024-03-08 00:44:34 -0300

    Adjust mapping Job -> Company

--- src/main/java/br/com/marciorodrigues/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/marciorodrigues/gestao_vagas/modules/company/entities/JobEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,10 +25,12 @@ public class JobEntity {
     private String level;
 
     @ManyToOne()
-    @JoinColumn(name = "company_id")
+    @JoinColumn(name = "company_id",
+    insertable = false,
+    updatable = false)
     private CompanyEntity companyEntity;
 
-   
+    @Column(name = "company_id")
     private UUID companyID;
     
     @CreationTimestamp
